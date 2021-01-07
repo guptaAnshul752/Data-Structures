5341e68294614b88e835d5d4c34c73fbe1c1ec7a
Anshul Gupta <guptaanshul752@gmail.com>
2021-01-07 23:43:57 +0530

    Fibonacci.cpp

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,23 @@
+/* Fibonacci series using Recursion */
+/* Emphasis is laid on Base case in Recursion according to PMI */
+/* Use PMI logic to form recursive codes */
+
+#include<iostream>
+
+using namespace std;
+
+int fibo(int n)
+{
+    if(n<=1) // Base Condition
+        return n;
+    return fibo(n-1)+fibo(n-2);
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number :"<<endl;
+    cin>>n;
+    cout<<"Fibo number is: "<<fibo(n);
+}
+
