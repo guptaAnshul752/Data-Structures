c6ef5564056bd9e6ffd89ad91ee2e896cbe75330
Anshul Gupta <guptaanshul752@gmail.com>
2021-01-15 19:26:39 +0530

    Add files via upload

---
+++ Stacks/ReverseStack.cpp
@@ -0,0 +1,42 @@
+// Reverse Stack
+// Print Stack Elements in Reversed Order
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void reverseStack(stack<int> &input, stack<int> &extra) {
+
+	int len = input.size();
+
+    for(int i=0;i<len;i++){
+        extra.push(input.top());
+        input.pop();
+    }
+
+    input = extra;
+}
+
+int main(){
+
+    stack<int> s1, s2;
+    cout << "Enter Size of Stack: ";
+    int size;
+    cin >> size;
+
+    cout << "Enter the elements in Stack: ";
+    int val;
+    for(int i = 0; i < size; i++) {
+    	cin >> val;
+    	s1.push(val);
+    }
+
+    reverseStack(s1, s2);
+
+    cout << "Elements Printed in Reversed Manner are: ";
+    while(!s1.empty()) {
+    	cout << s1.top() << " ";
+    	s1.pop();
+    }
+}
