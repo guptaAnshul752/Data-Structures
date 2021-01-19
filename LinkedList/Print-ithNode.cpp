1770ff2bf60a0827d3b852de97107b8dd263ec3e
Anshul Gupta <guptaanshul752@gmail.com>
2021-01-19 21:56:58 +0530

    Print node at Ith Position
    Given a linked list and a position i, print the node at ith position.
    If position i is greater than length of LL, then don't print anything.
    Indexing starts from 0.
    Input format :
    Line 1: Linked list elements (separated by space and terminated by -1)
    Line 2: Integer I (position)
    Output format :
    Element at the ith position

---
+++ LinkedList/Print-ithNode.cpp
@@ -0,0 +1,67 @@
+// Linked List
+// Print ith Node
+
+#include <iostream>
+using namespace std;
+
+// Linked List Node Class
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+Node* takeinput() {
+    int data;
+    cin >> data;
+    Node* head = NULL, *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL)                  {
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print (Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+void printIthNode (Node *head, int i) {
+    Node *temp = head;
+    int len =0;
+    while(temp!=NULL){
+        if(len==i){
+            cout<<temp->data;
+        }
+        len++;
+        temp=temp->next;
+    }    
+}
+
+
+int main() {
+    Node *head = takeinput();
+    int pos;
+    cout << "Enter Position: ";
+    cin >> pos;
+    cout << "Node at position " << pos << " is ";
+    printIthNode(head, pos);
+    return 0;
+}
