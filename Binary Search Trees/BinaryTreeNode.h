7cb20d513239c4ca6234b7a210b82aa1860aa76c
Anshul Gupta <guptaanshul752@gmail.com>
2021-02-15 11:33:13 +0530

    Binary Tree Node Class

---
+++ Binary Search Trees/BinaryTreeNode.h
@@ -0,0 +1,26 @@
+// BinaryTreeNode class
+
+template <typename T>
+
+class BinaryTreeNode{
+
+    public:
+        T data;
+        BinaryTreeNode* left;
+        BinaryTreeNode* right;
+
+        // Constructor
+        BinaryTreeNode(T data){
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+        }
+
+        // Destructor
+        // First deletes the child den root
+
+        ~BinaryTreeNode(){
+        delete left;
+        delete right;
+        }
+};
