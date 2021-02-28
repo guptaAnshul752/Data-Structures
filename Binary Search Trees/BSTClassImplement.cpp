09046b0ddeb72ce41a62a4279a1a5070e8e83d9a
Anshul Gupta <guptaanshul752@gmail.com>
2021-02-28 14:20:59 +0530

    Create BSTClassImplement.cpp

---
+++ Binary Search Trees/BSTClassImplement.cpp
@@ -0,0 +1,49 @@
+// BST Class Implementation
+
+#include<iostream>
+#include "BSTClass.h" // Important
+
+using namespace std;
+
+int main(){
+
+    BST b;
+    b.insertData(10);
+    b.insertData(5);
+    b.insertData(20);
+    b.insertData(7);
+    b.insertData(3);
+    b.insertData(15);
+    b.printTree();
+    b.deleteData(10);
+    b.printTree();
+    /*
+    BST *tree = new BST();
+    int choice, input;
+    while(true){
+        cin>>choice;
+        switch(choice){
+            case 1:
+                cin >> input;
+                tree->insertData(input);
+                break;
+            case 2:
+                cin >> input;
+                tree->deleteData(input);
+                break;
+            case 3:
+                cin >> input;
+                if(tree->hasData(input)) {
+                    cout << "true" << endl;
+                }
+                else {
+                    cout << "false" << endl;
+                }
+                break;
+            default:
+                tree->printTree();
+                return 0;
+                break;
+        }
+        */
+    }
